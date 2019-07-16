9fc90f1b7b076c0d425378beaca7994dfc3a1fda
canhui <1228923946@qq.com>
2019-07-16 22:31:54 +0800

    content

--- Source_Code/gRPC_module/test3.go
+++ Source_Code/gRPC_module/test3.go
@@ -1,21 +1,254 @@
-package main
-import "fmt"
+// package main
+// import "fmt"
 
-func printHello(ch chan int) {
-    fmt.Println("hi, this is from printHello()")
-    ch <- 2 // send a value (2) to the channel
+// func printHello(ch chan int) {
+//     fmt.Println("hi, this is from printHello()")
+//     ch <- 2 // send a value (2) to the channel
+// }
+
+// func main() {
+//     ch := make(chan int) // make a channel
+
+//     go func() {
+//         fmt.Println("hi, this is from main.func()")
+//         ch <- 1 // send a value (1) to the channel
+//     }()
+
+//     go printHello(ch) // send a value (2) to the channel
+    
+//     i := <- ch // read first value (1 or 2) from the channel
+//     <- ch // read second value (2 or 1) from the channel
+//     close(ch) // channel队列之前写入两个值，也读取完两个值。所以，队列为空，这里可以关闭。
+//     fmt.Println("Received", i)   
+// }
+
+
+
+
+//-----------------------------------------------------------------------
+
+// package main
+
+// import (
+//     "context"
+//     //"log"
+//     //"os"
+//     "fmt"
+//     "time"
+// )
+
+// //var logg *log.Logger
+
+// func someHandler() {
+//     ctx, cancel := context.WithCancel(context.Background())
+    
+//     go doStuff(ctx)
+
+// 	//10秒后取消doStuff
+//     time.Sleep(5 * time.Second)
+//     cancel()
+
+// }
+
+// //每1秒work一下，同时会判断ctx是否被取消了，如果是就退出
+// func doStuff(ctx context.Context) {
+//     for {
+//         time.Sleep(1 * time.Second)
+//         select {
+//         case <-ctx.Done():
+//         	fmt.Println("done")
+//             //logg.Printf("done")
+//             return
+//         default:
+//         	fmt.Println("work")
+//             //logg.Printf("work")
+//         }
+//     }
+// }
+
+// func main() {
+//     //logg = log.New(os.Stdout, "", log.Ltime)
+//     someHandler()
+//     //logg.Printf("down")
+// }
+
+// ------------------------------------------------------------------
+
+// package main
+
+// import "fmt"
+// import "golang.org/x/net/context"
+
+// // A message processes parameter and returns the result on responseChan.
+// // ctx is places in a struct, but this is ok to do.
+// type message struct {
+// 	responseChan chan<- int
+// 	parameter    string
+// 	ctx          context.Context
+// }
+
+// func ProcessMessages(work <-chan message) {
+// 	for job := range work {
+// 		select {
+// 		// If the context is finished, don't bother processing the
+// 		// message.
+// 		case <-job.ctx.Done():
+// 			continue
+// 		default:
+// 		}
+// 		// Assume this takes a long time to calculate
+// 		hardToCalculate := len(job.parameter)
+// 		select {
+// 		case <-job.ctx.Done():
+// 		case job.responseChan <- hardToCalculate:
+// 		}
+// 	}
+// }
+
+// func newRequest(ctx context.Context, input string, q chan<- message) {
+// 	r := make(chan int)
+// 	select {
+// 	// If the context finishes before we can send msg onto q,
+// 	// exit early
+// 	case <-ctx.Done():
+// 		fmt.Println("Context ended before q could see message")
+// 		return
+// 	case q <- message{
+// 		responseChan: r,
+// 		parameter:    input,
+// 		// We are placing a context in a struct.  This is ok since it
+// 		// is only stored as a passed message and we want q to know
+// 		// when it can discard this message
+// 		ctx: ctx,
+// 	}:
+// 	}
+
+// 	select {
+// 	case out := <-r:
+// 		fmt.Printf("The len of %s is %d\n", input, out)
+// 	// If the context finishes before we could get the result, exit early
+// 	case <-ctx.Done():
+// 		fmt.Println("Context ended before q could process message")
+// 	}
+// }
+
+// func main() {
+// 	q := make(chan message)
+// 	go ProcessMessages(q)
+// 	ctx := context.Background()
+// 	newRequest(ctx, "hi", q)
+// 	newRequest(ctx, "hello", q)
+// 	close(q)
+// }
+
+// ----------------------------------------------------------------
+// package main 
+ 
+// import (
+//     "fmt"
+//     "context"
+//     "time"
+// )
+ 
+// type Context interface {
+//     Deadline() (deadline time.Time, ok bool)
+//     Done() <-chan struct{}
+//     Err() error
+//     Value(key interface{}) interface{}
+// }
+
+// func contextDemo(name string, ctx context.Context) {    
+//     for {
+//         if ok {
+//             fmt.Println(name, "will expire at:", deadline)
+//         } else {
+//             fmt.Println(name, "has no deadline")
+//         }
+//         time.Sleep(time.Second)
+//     }
+// }
+
+// func main() {
+//     timeout := 3 * time.Second
+//     deadline := time.Now().Add(4 * time.Hour)
+//     timeOutContext, _ := context.WithTimeout(
+//         context.Background(), timeout)
+//     cancelContext, cancelFunc := context.WithCancel(
+//         context.Background())
+//     deadlineContext, _    := context.WithDeadline(
+//         cancelContext, deadline)
+ 
+ 
+//     go contextDemo("[timeoutContext]", timeOutContext)
+//     go contextDemo("[cancelContext]", cancelContext)
+//     go contextDemo("[deadlineContext]", deadlineContext)
+ 
+//     // Wait for the timeout to expire
+//     <- timeOutContext.Done()
+ 
+//     // This will cancel the deadline context as well as its
+//     // child - the cancelContext
+//     fmt.Println("Cancelling the cancel context...")
+//     cancelFunc()
+ 
+//     <- cancelContext.Done()
+//     fmt.Println("The cancel context has been cancelled...")
+ 
+//     // Wait for both contexts to be cancelled
+//     <- deadlineContext.Done()
+//     fmt.Println("The deadline context has been cancelled...")
+// }
+
+// -----------------------------------------------------------------
+
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+func withTimeOut() {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
+
+	sleepAndTalk(ctx, 5*time.Second, "Hello withTimeOut!")
+}
+
+func withCancel() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+
+	go func() {
+		s := bufio.NewScanner(os.Stdin)
+		s.Scan()
+		cancel()
+	}()
+
+	sleepAndTalk(ctx, 5*time.Second, "Hello withCancel!")
+}
+
+func background() {
+	ctx := context.Background()
+	sleepAndTalk(ctx, 5*time.Second, "Hello background!")
+}
+
+func sleepAndTalk(ctx context.Context, d time.Duration, s string) {
+	select {
+	case <-time.After(d):
+		fmt.Println(s)
+	case <-ctx.Done():
+		log.Println(ctx.Err())
+	}
 }
 
 func main() {
-    ch := make(chan int) // make a channel
-
-    go func() {
-        fmt.Println("hi, this is from main.func()")
-        ch <- 1 // send a value (1) to the channel
-    }()
-
-    go printHello(ch)
-    i := <- ch
-    fmt.Println("Received", i)
-    <- ch
+	background()
+	withCancel()
+	withTimeOut()
 }
--- Source_Code/gRPC_module/test3.md
+++ Source_Code/gRPC_module/test3.md
@@ -56,6 +56,8 @@ func main() {
 }
 ```
 
+运行
+
 ```shell
 joe@ubuntu01:~/Public/Coding_Storage/Source_Code/gRPC_module$ go run test3.go
 hi, this is from main
@@ -99,12 +101,51 @@ func main() {
         ch <- 1 // send a value (1) to the channel
     }()
 
-    go printHello(ch)
-    i := <- ch
-    fmt.Println("Received", i)
-    <- ch
+    go printHello(ch) // send a value (2) to the channel
+    
+    i := <- ch // read first value (1 or 2) from the channel
+    <- ch // read second value (2 or 1) from the channel
+    close(ch) // channel队列之前写入两个值，也读取完两个值。所以，队列为空，这里可以关闭。
+    fmt.Println("Received", i)   
 }
 ```
+
+运行
+
+```shell
+$ go run test3.go
+hi, this is from printHello()
+Received 2
+hi, this is from main.func()
+$ go run test3.go
+hi, this is from main.func()
+hi, this is from printHello()
+Received 1
+```
+
+<br />
+<br />
+
+## 2. Context
+
+Context被设计用于管理(比如中止)Goroutine线程。举个例子，如果某一个线程启动一个Goroutine线程，该Goroutine线程运行太久太慢。此时，如果用户可以通过Context中止该线程。Context提供下列几种类型。
+
+<br />
+
+
+#### 2.1. context.Background()
+
+该方法返回一个空的context，用于派生其他类型的context。
+
+```go
+ctx := context.Background()
+```
+
+
+
+
+
+
 
 
 
@@ -116,4 +157,10 @@ func main() {
 <br />
 
 ## 参考资料
-[1. Golang context package的理解] http://p.agnihotry.com/post/understanding_the_context_package_in_golang/
+[1. Golang channel tutorial] https://tour.golang.org/concurrency/2
+[2. Golang context package的理解] http://p.agnihotry.com/post/understanding_the_context_package_in_golang/
+[3. Golang context examples] https://www.jianshu.com/p/d24bf8b6c869
+[4. Golang context more examples] https://medium.com/@cep21/how-to-correctly-use-context-context-in-go-1-7-8f2c0fafdf39
+
+[5. Golang context例子] https://www.sohamkamani.com/blog/golang/2018-06-17-golang-using-context-cancellation/
+[6. Golang content例子] https://gist.github.com/astenmies/2f180213d310aa16b4b8c4e637be9441
