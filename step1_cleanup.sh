0f8c8f76cf1219db6af8fa20c03283458598f898
canhui <1228923946@qq.com>
2019-09-26 15:13:30 +0800

    source

---
+++ step1_cleanup.sh
@@ -0,0 +1,4 @@
+rm -rf $HOME/fabric-dbench/orderer.example.com
+rm -rf $HOME/fabric-dbench/peer0.*
+rm -rf $HOME/fabric-dbench/certs
+rm -rf $HOME/fabric-dbench/run
---
+++ step1_config_cluster.sh
@@ -0,0 +1,9 @@
+$HOME/fabric-dbench/source/step1_1.sh
+$HOME/fabric-dbench/source/step1_2.sh
+$HOME/fabric-dbench/source/step1_3.sh
+$HOME/fabric-dbench/source/step1_4.sh
+$HOME/fabric-dbench/source/step1_5.sh
+$HOME/fabric-dbench/source/step1_6.sh
+$HOME/fabric-dbench/source/step1_7.sh
+$HOME/fabric-dbench/source/step1_8.sh
+
