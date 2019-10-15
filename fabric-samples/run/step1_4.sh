a980bfc1923839d630a04cf3a8958a2d57865a85
canhui <1228923946@qq.com>
2019-10-15 09:55:36 +0800

    samples

--- fabric-samples/run/step1_4.sh
+++
@@ -1,8 +0,0 @@
-echo "------------------------------------------------------------"
-echo "Build the orderer configuration files"
-echo "------------------------------------------------------------"
-mkdir /home/t716/fabric-dbench/fabric-samples/orderer1.example.com
-cp /home/t716/fabric-dbench/fabric-samples/bin/orderer /home/t716/fabric-dbench/fabric-samples/orderer1.example.com/
-cp -rf /home/t716/fabric-dbench/fabric-samples/certs/ordererOrganizations/example.com/orderers/orderer1.example.com/* /home/t716/fabric-dbench/fabric-samples/orderer1.example.com/
-cp /home/t716/fabric-dbench/fabric-samples/configyaml/orderer.yaml /home/t716/fabric-dbench/fabric-samples/orderer1.example.com/
-mkdir /home/t716/fabric-dbench/fabric-samples/orderer1.example.com/data
--- fabric-samples/run2_config_admins.sh
+++
@@ -1,1 +0,0 @@
-/home/t716/fabric-dbench/fabric-samples/run/step2_1.sh
